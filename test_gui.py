1f95a8fe1dd4195286d1c703956e93a1711a008a
Trent Zaranek <tzaranek@umich.edu>
2018-01-30 14:55:34 -0500

    Class to wrap Tkinter implemented

---
+++ test_gui.py
@@ -0,0 +1,22 @@
+from threading import Thread
+from gui import *
+from time import sleep
+
+#Hacky way to get interactive python shell while GUI is running
+#After running any tests you want in this function
+#Control-C (or whatever it is on mac) and then hover the gui
+#You should get a keyboard interrupt in the interpreter and then
+#You can hit enter to start an interactive session
+def runTests(gui):
+    sleep(2)
+    gui.textMode()
+    sleep(2)
+    gui.commandMode()
+    
+#The GUI library does NOT like it if you run the GUI from anything
+#other than the main thread, so hopefully we can 
+if __name__ == "__main__":
+    gui = GUIClass()
+    thread = Thread(target = runTests, args = [gui])
+    thread.start()
+    gui.start()
