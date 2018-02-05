62080a16e322ad8843a05d53a4d27147b6577a38
Wu <ljwu@UMICH.EDU>
2018-02-05 15:02:27 -0500

    Continue work on parsing engine
    * Alt-tab and hold seems to work in ideal testing

--- state.py
+++ state.py
@@ -1,130 +1,195 @@
-from enum import Enum
 import time
 import re
-import win32gui, win32con, win32api, win32process
+import json
+import struct
+import sys
+import win32gui, win32con, win32api
+from keyboard import KeyboardEvent
+from window_properties import currentApp
 
-def getVisibleWindows():
-    def enumHandler(hwnd, results):
-        if not win32gui.IsWindowVisible(hwnd):
-            return
-        results.append(hwnd)
-    handles = []
-    win32gui.EnumWindows(enumHandler, handles)
-    return handles
-            
-
-def currentApp():
-    return win32gui.GetWindowText(win32gui.GetForegroundWindow())
-
-def getFileProperties(fname):
+class EncoderOverload(json.JSONEncoder):
     """
-    Read all properties of the given file return them as a dictionary.
+    JSONEncoder subclass that leverages an object's `__json__()` method,
+    if available, to obtain its default JSON representation. 
+
     """
-    propNames = ('Comments', 'InternalName', 'ProductName',
-        'CompanyName', 'LegalCopyright', 'ProductVersion',
-        'FileDescription', 'LegalTrademarks', 'PrivateBuild',
-        'FileVersion', 'OriginalFilename', 'SpecialBuild')
+    def default(self, obj):
+        if hasattr(obj, '__json__'):
+            return obj.__json__()
+        return json.JSONEncoder.default(self, obj)
 
-    props = {'FixedFileInfo': None, 'StringFileInfo': None, 'FileVersion': None}
 
+class KeyboardMessage():
+    def __init__(self, key, repeat=False, shiftKey=False, ctrlKey=False, altKey=False, metaKey=False):
+        self.message = {
+            'key': key,
+            'repeat': repeat,
+            'shiftKey': shiftKey,
+            'ctrlKey': ctrlKey,
+            'altKey': altKey,
+            'metaKey': metaKey
+        }
+
+    def __json__(self):
+        return self.message
+
+browserKeywords = {
+    "UP": KeyboardMessage('j'),
+    "DOWN": KeyboardMessage('k'),
+    "LEFT": KeyboardMessage('h'),
+    "RIGHT": KeyboardMessage('l'),
+    "CONTROL UP": KeyboardMessage('u', ctrlKey=True),
+    "CONTROL DOWN": KeyboardMessage('d', ctrlKey=True),
+    "CONTROL UPPER": KeyboardMessage('b', ctrlKey=True),
+    "CONTROL DOWNER": KeyboardMessage('f', ctrlKey=True),
+
+    "0": KeyboardMessage('0'),
+    "ZERO": KeyboardMessage('0'),
+    "DOLLAR": KeyboardMessage('$'), # TODO: does this recognize dollar?
+
+    "TOP": KeyboardMessage('gg'),
+    "BOTTOM": KeyboardMessage('G')
+
+    # TODO: rest of messages
+}
+
+# source: https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Native_messaging
+def encode_message(message_content):
+    encoded_content = json.dumps(message_content, cls=EncoderOverload)
+    encoded_length = struct.pack('@I', len(encoded_content))
+    return {'length': encoded_length, 'content': encoded_content}
+
+
+# source: https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Native_messaging
+def send_message(encoded_message):
     try:
-        # backslash as parm returns dictionary of numeric info corresponding to VS_FIXEDFILEINFO struc
-        fixedInfo = win32api.GetFileVersionInfo(fname, '\\')
-        props['FixedFileInfo'] = fixedInfo
-        props['FileVersion'] = "%d.%d.%d.%d" % (fixedInfo['FileVersionMS'] / 65536,
-                fixedInfo['FileVersionMS'] % 65536, fixedInfo['FileVersionLS'] / 65536,
-                fixedInfo['FileVersionLS'] % 65536)
-
-        # \VarFileInfo\Translation returns list of available (language, codepage)
-        # pairs that can be used to retreive string info. We are using only the first pair.
-        lang, codepage = win32api.GetFileVersionInfo(fname, '\\VarFileInfo\\Translation')[0]
-
-        # any other must be of the form \StringfileInfo\%04X%04X\parm_name, middle
-        # two are language/codepage pair returned from above
-
-        strInfo = {}
-        for propName in propNames:
-            strInfoPath = u'\\StringFileInfo\\%04X%04X\\%s' % (lang, codepage, propName)
-            ## print str_info
-            strInfo[propName] = win32api.GetFileVersionInfo(fname, strInfoPath)
-
-        props['StringFileInfo'] = strInfo
-    except:
-        pass
-
-    return props
-
-
-def getApplicationName(hwnd):
-    (_threadId, processId) = win32process.GetWindowThreadProcessId(hwnd)
-    pHandle = win32api.OpenProcess(
-                win32con.PROCESS_QUERY_INFORMATION |
-                win32con.PROCESS_VM_READ, 0, processId)
-    pExeName = win32process.GetModuleFileNameEx(pHandle, 0)
-    props = getFileProperties(pExeName)
-    return props['StringFileInfo']['FileDescription']
+        # python 2.7 compatible - supported because firefox insists on using v2
+        sys.stdout.write(encoded_message['length'])
+        sys.stdout.write(encoded_message['content'].encode())
+        sys.stdout.flush()
+    except TypeError:
+        # python 3 compatible
+        sys.stdout.buffer.write(encoded_message['length'])
+        sys.stdout.buffer.write(encoded_message['content'].encode())
+        sys.stdout.flush()
 
 
 class state:
     def __init__(self):
         # Define bits for modes we can be in
         self.NORMAL     = 2**0
-        self.ALT        = 2**1
-        self.CTRL       = 2**2
-        self.INSERT     = 2**3
+        self.HOLDING    = 2**1
+        self.INSERT     = 2**2
         self.CLEAR      = 0
 
         self.mode = self.NORMAL
+
+        self.held = set()
+
         self.commands = {
-                "control": {
-                    "tab": self.changeTab
-                },
-                "caps": {
-                    "lock": self.switchMode
-                },
-                "alt": {
-                    "tab": self.changeApps
-                }
+            "ALT": self.parseAlt,
+            "HOLD": self.parseHold,
+            "RESIZE": self.parseResize,
+            "ESCAPE": self.parseEscape
         }
 
-    def switchMode(self, _):
+
+    def switchMode(self):
+        print("Switching modes")
         self.mode = self.INSERT if self.mode & self.NORMAL else self.NORMAL
 
-    def changeApps(self, appName):
-        print(getApplicationName(win32gui.GetForegroundWindow()))
-        windows = getVisibleWindows()
-        for hwnd in windows:
-            if ' '.join(appName) == getApplicationName(hwnd):
-                print(hwnd)
-                win32gui.BringWindowToTop(hwnd)
-                win32gui.SetForegroundWindow(hwnd)
 
-    def changeTab(self, _):
-        browsers = ["Google Chrome", "Firefox"]
-        if currentApp()[0] in browsers:
-            print("Forwarding...")
+    def parseAlt(self, tokens):
+        if tokens[0] == 'TAB':
+            KeyboardEvent.pressSequence(['ALT', 'TAB'])
         else:
-            print("awef")
+            print("Parsing ALT command failed. Could not recognize: {}".format(tokens[0]))
 
-    def curMode(self):
-        return self.mode
+        self.recursiveParse(tokens[1:])
 
-    def recursiveParse(self, words, levelDict):
-        w, rest = words[0], words[1:]
+
+    def parseHold(self, tokens):
+        def clearHeld():
+            self.mode &= ~self.HOLDING
+            for key in self.held:
+                KeyboardEvent.keyUp(key)
+
+        self.mode |= self.HOLDING
+        for token in tokens:
+            if token == 'ESCAPE':
+                clearHeld()
+                self.recursiveParse(tokens[tokens.index(token)+1:])
+                return
+
+            if KeyboardEvent.keyDown(token):
+                self.held.add(token)
+            else:
+                print("Parsing HOLD command failed. Could not recognize: {}".format(token))
+                clearHeld()
+                self.recursiveParse(tokens[tokens.index(token)+1:])
+                return
+
+
+    def parseResize(self, tokens):
+        if tokens[0] == 'LEFT':
+            pass
+        elif tokens[0] == 'RIGHT':
+            pass
+        elif tokens[0] == 'UP':
+            pass
+        elif tokens[0] == 'DOWN':
+            pass
+        elif tokens[0] == 'FULL':
+            pass
+        else:
+            print("Parsing RESIZE command failed. Could not recognize: {}".format(tokens[0]))
+
+        self.recursiveParse(tokens[1:])
+
+
+    def parseEscape(self, tokens):
+        self.switchMode()
+        self.recursiveParse(tokens)
+
+
+    def forwardBrowser(self, tokens):
+        tokenStr = ' '.join(tokens)
+        if tokenStr in browserKeywords:
+            send_message(encode_message(browserKeywords[tokenStr]))
+
+
+    def recursiveParse(self, tokens, levelDict = None):
+        if not tokens:
+            return
+
+        if self.mode & self.HOLDING:
+            self.parseHold(tokens)
+            return
+
+        if levelDict == None:
+            levelDict = self.commands
+
+        w, rest = tokens[0], tokens[1:]
         if w in levelDict:
             if isinstance(levelDict[w], dict):
                 self.recursiveParse(rest, levelDict[w])
             else:
                 levelDict[w](rest)
         else:
-            print("Command not found")
+            # TODO: change this for testing w/ firefox
+            if currentApp() == 'Google Chrome':
+                self.forwardBrowser(tokens)
+            else:
+                print("Command not found")
 
 
     def parse(self, command):
+        command = command.strip().upper()
         if self.mode & self.NORMAL:
             text = re.findall(r"[a-zA-Z]+", command)
-            print(text)
-            self.recursiveParse(text, self.commands)
+            print("Tokens parsed: {}".format(text))
+
+            self.recursiveParse(text)
         else:
             if command == "caps lock":
                 self.switchMode(command)
@@ -132,12 +197,12 @@ class state:
                 print("Sending: \"{}\" to top application".format(command))
 
 v = state()
-# time.sleep(2)
 v.parse("alt tab")
-v.parse("alt-tab")
-v.parse("control tab")
-v.parse("caps lock")
-v.parse("control-tab")
-v.parse("alt-tab")
-v.parse("caps lock")
-v.parse("alt-tab Google Chrome")
+v.parse("hold alt")
+time.sleep(1)
+v.parse("tab")
+time.sleep(1)
+v.parse("tab")
+time.sleep(1)
+v.parse("escape")
+v.parse("control up")
