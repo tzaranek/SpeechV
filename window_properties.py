8d82102bc89519c836ee3fb3875f85d537288497
Wu <ljwu@UMICH.EDU>
2018-02-05 15:01:29 -0500

    Move process name calculations to separate file

---
+++ window_properties.py
@@ -0,0 +1,55 @@
+import win32api, win32con, win32process, win32gui
+
+def getFileProperties(fname):
+    """
+    Read all properties of the given file return them as a dictionary.
+    """
+    propNames = ('Comments', 'InternalName', 'ProductName',
+        'CompanyName', 'LegalCopyright', 'ProductVersion',
+        'FileDescription', 'LegalTrademarks', 'PrivateBuild',
+        'FileVersion', 'OriginalFilename', 'SpecialBuild')
+
+    props = {'FixedFileInfo': None, 'StringFileInfo': None, 'FileVersion': None}
+
+    try:
+        # backslash as parm returns dictionary of numeric info corresponding to VS_FIXEDFILEINFO struc
+        fixedInfo = win32api.GetFileVersionInfo(fname, '\\')
+        props['FixedFileInfo'] = fixedInfo
+        props['FileVersion'] = "%d.%d.%d.%d" % (fixedInfo['FileVersionMS'] / 65536,
+                fixedInfo['FileVersionMS'] % 65536, fixedInfo['FileVersionLS'] / 65536,
+                fixedInfo['FileVersionLS'] % 65536)
+
+        # \VarFileInfo\Translation returns list of available (language, codepage)
+        # pairs that can be used to retreive string info. We are using only the first pair.
+        lang, codepage = win32api.GetFileVersionInfo(fname, '\\VarFileInfo\\Translation')[0]
+
+        # any other must be of the form \StringfileInfo\%04X%04X\parm_name, middle
+        # two are language/codepage pair returned from above
+
+        strInfo = {}
+        for propName in propNames:
+            strInfoPath = u'\\StringFileInfo\\%04X%04X\\%s' % (lang, codepage, propName)
+            ## print str_info
+            strInfo[propName] = win32api.GetFileVersionInfo(fname, strInfoPath)
+
+        props['StringFileInfo'] = strInfo
+    except:
+        pass
+
+    return props
+
+
+def getApplicationName(hwnd):
+    (_threadId, processId) = win32process.GetWindowThreadProcessId(hwnd)
+    pHandle = win32api.OpenProcess(
+                win32con.PROCESS_QUERY_INFORMATION |
+                win32con.PROCESS_VM_READ, 0, processId)
+    pExeName = win32process.GetModuleFileNameEx(pHandle, 0)
+    props = getFileProperties(pExeName)
+    return props['StringFileInfo']['FileDescription']
+
+
+def currentApp():
+    return getApplicationName(win32gui.GetForegroundWindow())
+
+
