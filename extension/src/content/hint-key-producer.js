ab48eb7ca78bcab478cd86c76185ff19de0d26de
Lawrence <lawrencejwu@gmail.com>
2018-04-03 11:53:29 -0400

    Generate a prefix-free list for follow
    * Replaces follow hint generation by using sequences such that no hint
      is the prefix is another and thus the user will never have to enter a
      sequence of characters and then 'enter'

--- extension/src/content/hint-key-producer.js
+++ extension/src/content/hint-key-producer.js
@@ -1,33 +1,87 @@
+const replaceAt = (str, index, replacement) => {
+    return str.substr(0, index)
+        + replacement
+        + str.substr(index + replacement.length);
+};
+
+function* sequenceGenerator (charset) {
+  if (charset.length === 0) {
+    throw new TypeError('charset is empty');
+  }
+
+  let seq = "";
+  let seqIdx = 0;
+  while (true) {
+    let curCharIdx = charset.indexOf(seq[seqIdx]);
+    if (seqIdx !== -1 && curCharIdx === charset.length - 1) {
+      seq = replaceAt(seq, seqIdx, charset[0]);
+      seqIdx -= 1;
+    } else {
+      seq = seqIdx === -1
+            ? charset[0] + seq
+            : replaceAt(seq, seqIdx, charset[curCharIdx+1]);
+      seqIdx = seq.length - 1;
+      yield seq;
+    }
+  }
+};
+
+class HintTree {
+  // each tree holds it's top-level nodes and pointers to subtrees
+
+  constructor(value) {
+    this.children = {};
+    // this node itself is currently a leaf but for ease of counting, don't
+    // count yourself as a leaf
+    this.leaves = 0;
+    this.value = value;
+  }
+
+  addHint(sequence) {
+    let newLeafCount = 1;
+    if (this.leaves === 0) {
+      // parent's don't get any new leaves since the current node is a leaf and
+      // we are just moving it's leafy status to its child. however, the node
+      // itself must recognize for counting that it should increment it's
+      // leaves since it is constructed to 0
+      newLeafCount = 0;
+      this.leaves = 1;
+    }
+    if (!(sequence[0] in this.children)) {
+      this.children[sequence[0]] = new HintTree(sequence[0]);
+    }
+    if (sequence.length !== 1) {
+      newLeafCount = this.children[sequence[0]].addHint(sequence.substring(1));
+    }
+    this.leaves += newLeafCount;
+    return newLeafCount;
+  }
+
+  getLeaves() {
+    let ret = this.leaves === 0 ? [this.value] : [];
+    Object.keys(this.children).forEach((key) => {
+      let subSeqs = this.children[key].getLeaves();
+      subSeqs.forEach((s) => ret.push(this.value + s));
+    });
+    return ret;
+  }
+}
+
 export default class HintKeyProducer {
   constructor(charset) {
     if (charset.length === 0) {
       throw new TypeError('charset is empty');
     }
-
     this.charset = charset;
-    this.counter = [];
   }
 
-  produce() {
-    this.increment();
-
-    return this.counter.map(x => this.charset[x]).join('');
-  }
-
-  increment() {
-    let max = this.charset.length - 1;
-    if (this.counter.every(x => x === max)) {
-      this.counter = new Array(this.counter.length + 1).fill(0);
-      return;
+  produce(count) {
+    let tree = new HintTree("");
+    let gen = sequenceGenerator(this.charset);
+    while (tree.leaves < count) {
+      let nextSeq = gen.next();
+      tree.addHint(nextSeq.value);
     }
-
-    this.counter.reverse();
-    let len = this.charset.length;
-    let num = this.counter.reduce((x, y, index) => x + y * len ** index) + 1;
-    for (let i = 0; i < this.counter.length; ++i) {
-      this.counter[i] = num % len;
-      num = ~~(num / len);
-    }
-    this.counter.reverse();
+    return tree.getLeaves();
   }
 }
